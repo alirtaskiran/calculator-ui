5accd1e5ffe8ece5756426033d00eb4d07f1d5b4
alirtaskiran <alirtaskiran@gmail.com>
2024-02-20 01:16:33 +0300

    added some conditions

--- app.js
+++ app.js
@@ -11,26 +11,41 @@ addition.addEventListener("click", () => {
   let firstNumber = document.getElementById("firstNumber").value;
   let secondNumber = document.getElementById("secondNumber").value;
   let newResult = parseFloat(firstNumber) + parseFloat(secondNumber);
-  document.getElementById("result").textContent = "Sonuç: " + newResult.toFixed(3);
+  document.getElementById("result").textContent =
+    "Sonuç: " + newResult.toFixed(3);
 });
 
 subtraction.addEventListener("click", () => {
   let firstNumber = parseFloat(document.getElementById("firstNumber").value);
   let secondNumber = parseFloat(document.getElementById("secondNumber").value);
   let newResult = parseFloat(firstNumber) - parseFloat(secondNumber);
-  document.getElementById("result").textContent = "Sonuç: " + newResult.toFixed(3);
+  document.getElementById("result").textContent =
+    "Sonuç: " + newResult.toFixed(3);
 });
 
 multiplication.addEventListener("click", () => {
   let firstNumber = parseFloat(document.getElementById("firstNumber").value);
   let secondNumber = parseFloat(document.getElementById("secondNumber").value);
-  let newResult = parseFloat(firstNumber) * parseFloat(secondNumber);
-  document.getElementById("result").textContent = "Sonuç: " + newResult.toFixed(3);
+  if (firstNumber === 0 || secondNumber === 0) {
+    document.getElementById("result").textContent =
+      "Sıfırla bir sayıyı çarparsan sonuç 0 çıkar.";
+  } else {
+    let newResult = parseFloat(firstNumber) * parseFloat(secondNumber);
+    document.getElementById("result").textContent =
+      "Sonuç: " + newResult.toFixed(3);
+  }
 });
 
 division.addEventListener("click", () => {
   let firstNumber = parseFloat(document.getElementById("firstNumber").value);
   let secondNumber = parseFloat(document.getElementById("secondNumber").value);
-  let newResult = parseFloat(firstNumber) / parseFloat(secondNumber);;
-  document.getElementById("result").textContent = "Sonuç: " + newResult.toFixed(3);
+  if (secondNumber === 0) {
+    document.getElementById("result").textContent =
+      "Bir sayıyı sıfıra bölemezsin.";
+    console.log("Bir sayıyı sıfıra bölemezsin.");
+  } else {
+    let newResult = parseFloat(firstNumber) / parseFloat(secondNumber);
+    document.getElementById("result").textContent =
+      "Sonuç: " + newResult.toFixed(3);
+  }
 });
